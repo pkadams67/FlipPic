cf331ffeeef06743dd39d8d0f7b454d6a21d52b8
“pkadams67” <paul.k.adams@me.com>
2016-03-07 20:38:37 -0700

    Reset all Git repositories

---
+++ Reacture/Reacture/RCT_AppDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  AppDelegate.swift
+//  FlipPic
+//
+//  Created by Andrew Porter on 1/5/16.
+//  Copyright © 2016 BAEPS. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        let firstLaunch = NSUserDefaults.standardUserDefaults().boolForKey("FirstLaunch")
+        if firstLaunch  {
+            print("Not first launch.")
+        }
+        else {
+            print("First launch, setting NSUserDefault.")
+           
+            self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            let initialViewController = storyboard.instantiateViewControllerWithIdentifier("TutorialView")
+            
+            self.window?.rootViewController = initialViewController
+            self.window?.makeKeyAndVisible()
+        }
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+    }
+}
