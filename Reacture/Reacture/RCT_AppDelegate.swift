6885a620c746d7d2003e66fa32d56987124cc9ee
“pkadams67” <paul@pkadams67.io>
2016-06-03 08:36:07 -0500

    Added Fabric debug logging

--- Reacture/Reacture/RCT_AppDelegate.swift
+++ Reacture/Reacture/RCT_AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
+        Fabric.sharedSDK().debug = true
         Fabric.with([Crashlytics.self])
         // TODO: Move this to where you establish a user session
         self.logUser()
